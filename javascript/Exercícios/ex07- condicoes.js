aa65cecbf87ca8036ced8d321b4a9baf9093739e
Eduardo Fonseca da Silva <edufon1503@gmail.com>
2022-06-16 12:49:27 -0300

    Mais exercicios

---
+++ javascript/Exercícios/ex07- condicoes.js
@@ -0,0 +1,15 @@
+var b = 'Brasil'
+if (b == 'Brasil') {
+    console.log(``)
+}
+var a = 102
+if(a > 25) {
+    if(a == 102) {
+        console.log(`Número legal`)
+    } else {
+        console.log(`Meh`)
+    }
+    console.log(`Número maior que 25`)
+} else {
+    console.log(`Número menor que 25`)
+}
